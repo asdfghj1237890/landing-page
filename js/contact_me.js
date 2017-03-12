cb9aefa3d58aa88ee78585ee04b5939f7767e396
asdfghj1237890 <asdfghj1237890@yahoo.com.hk>
2017-03-13 01:30:01 +0800

    contact email finish
    version 3.0

--- js/contact_me.js
+++ js/contact_me.js
@@ -12,19 +12,15 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
+            var opinion = $("textarea#opinion").val();
+
             $.ajax({
                 url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
                     email: email,
-                    message: message
+                    opinion: opinion
                 },
                 cache: false,
                 success: function() {
@@ -45,7 +41,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that my mail server is not responding. Please try again later!"));
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
